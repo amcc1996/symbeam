5f06da540973935f7d625733020aacff724f09b2
amcc1996 <amcc@fe.up.pt>
2020-06-30 19:25:51 +0100

    All examples with solve method

--- debug/debug_19_20_recurso.py
+++ debug/debug_19_20_recurso.py
@@ -5,10 +5,13 @@ print("\n ======================================================================
 print(" Debuggin script for Symbeam")
 print(" ============================================================================= ")
 
-test_beam = beam(1, x0=0)
-test_beam.add_support(0, 'pin')
-test_beam.add_support(1, 'roller')
-test_beam.add_point_load(0.5, -20)
+test_beam = beam(4, x0=0)
+test_beam.add_support(2, 'roller')
+test_beam.add_support(4, 'pin')
+test_beam.add_distributed_load(0, 2, '-5*x')
+test_beam.add_distributed_load(2, 4, '-(4*x**2-24*x+42)')
+test_beam.set_inertia(0, 4, 2.051e-5)
+test_beam.set_young(0, 4, 210e9)
 test_beam.solve()
 fig, ax = test_beam.plot()
 
