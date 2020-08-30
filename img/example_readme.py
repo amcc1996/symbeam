a85ab7308e84b5c03aa4ca40a56fc51a0634bee8
António Carneiro <amcc@fe.up.pt>
2020-08-30 14:51:47 +0100

    Added readme example

---
+++ img/example_readme.py
@@ -0,0 +1,28 @@
+from symbeam import beam
+from sympy.abc import L, E, I, P, M, q, x
+import matplotlib.pyplot as plt
+
+new_beam = beam(L)
+
+# new_beam.set_young(x_start, x_end, value)
+new_beam.set_young(0, L/2, E)
+new_beam.set_young(L/2, L, E/10)
+
+# new_beam.set_inertia(x_start, x_end, value)
+new_beam.set_inertia(0, L/2, I)
+new_beam.set_inertia(L/2, L, I/2)
+
+# new_beam.add_support(x_coord, type)
+new_beam.add_support(0, 'fixed')
+new_beam.add_support(L, 'roller')
+new_beam.add_support(3*L/4, 'hinge')
+
+new_beam.add_point_load(3*L/4, -P)
+new_beam.add_point_moment(L, M)
+new_beam.add_distributed_load(0, L/2, -q * x)
+
+new_beam.solve()
+
+new_beam.plot(subs={'P':1000, 'q':5000, 'L':2})
+
+plt.savefig(__file__.split('.py')[0]+'.svg')
