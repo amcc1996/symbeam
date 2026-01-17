f7be36430db5f0bd84f42afe1891ada764c38fae
António Carneiro <antonio.mccarneiro@gmail.com>
2026-01-17 12:57:52 +0000

    Fix bug with type hintint in older Python versions

--- symbeam/utils/utils.py
+++ symbeam/utils/utils.py
@@ -7,6 +7,8 @@ polynomials, and computation of rotation, bending moment, and shear force.
 ..moduleauthor:: A. M. Couto Carneiro <amcc@fe.up.pt>
 """
 
+from typing import List
+
 import sympy
 
 
@@ -15,8 +17,23 @@ x = sympy.symbols("x")
 
 def hermite_polynomials(
     x: sympy.Symbol, x0: sympy.Symbol, L: sympy.Symbol
-) -> list[sympy.Expr]:
-    """Returns the cubic Hermite polynomials."""
+) -> List[sympy.Expr]:
+    """Returns the cubic Hermite polynomials.
+
+    Parameters
+    ----------
+    x : sympy.Symbol
+        Independent variable.
+    x0 : sympy.Symbol
+        Element initial coordinate.
+    L : sympy.Symbol
+        Element length.
+
+    Returns
+    -------
+    N : List[sympy.Expr]
+        List of cubic Hermite polynomials.
+    """
     N1 = 1 - 3 * ((x - x0) / L) ** 2 + 2 * ((x - x0) / L) ** 3
     N2 = (x - x0) - 2 * ((x - x0) ** 2) / L + ((x - x0) ** 3) / (L**2)
     N3 = 3 * ((x - x0) / L) ** 2 - 2 * ((x - x0) / L) ** 3
@@ -28,7 +45,22 @@ def hermite_polynomials(
 def euler_bernoulli_stiff_matrix(
     E: sympy.Symbol, I: sympy.Symbol, L: sympy.Symbol
 ) -> sympy.Matrix:
-    """Returns the Euler-Bernoulli beam element stiffness matrix."""
+    """Returns the Euler-Bernoulli beam element stiffness matrix.
+
+    Parameters
+    ----------
+    E : sympy.Symbol
+        Young's modulus.
+    I : sympy.Symbol
+        Second moment of area.
+    L : sympy.Symbol
+        Element length.
+
+    Returns
+    -------
+    k : sympy.Matrix
+        Element stiffness matrix.
+    """
     k = (
         (E * I)
         / (L**3)
@@ -46,7 +78,20 @@ def euler_bernoulli_stiff_matrix(
 
 
 def compute_rotation(displacement: sympy.Expr, x: sympy.Symbol) -> sympy.Expr:
-    """Computes the rotation as the first derivative of the displacement."""
+    """Computes the rotation as the first derivative of the displacement.
+
+    Parameters
+    ----------
+    displacement : sympy.Expr
+        Displacement expression.
+    x : sympy.Symbol
+        Independent variable.
+
+    Returns
+    -------
+    rotation : sympy.Expr
+        Rotation expression.
+    """
     rotation = sympy.diff(displacement, x)
 
     return rotation
@@ -55,14 +100,44 @@ def compute_rotation(displacement: sympy.Expr, x: sympy.Symbol) -> sympy.Expr:
 def compute_bending_moment(
     displacement: sympy.Expr, E: sympy.Symbol, I: sympy.Symbol, x: sympy.Symbol
 ) -> sympy.Expr:
-    """Computes the bending moment as the second derivative of the displacement times EI."""
+    """Computes the bending moment as the second derivative of the displacement times EI.
+
+    Parameters
+    ----------
+    displacement : sympy.Expr
+        Displacement expression.
+    E : sympy.Symbol
+        Young's modulus.
+    I : sympy.Symbol
+        Second moment of area.
+    x : sympy.Symbol
+        Independent variable.
+
+    Returns
+    -------
+    bending_moment : sympy.Expr
+        Bending moment expression.
+    """
     bending_moment = E * I * sympy.diff(displacement, x, 2)
 
     return bending_moment
 
 
 def computes_shear_force(bending_moment: sympy.Expr, x: sympy.Symbol) -> sympy.Expr:
-    """Computes the shear force as the third derivative of the displacement times EI."""
+    """Computes the shear force as the third derivative of the displacement times EI.
+
+    Parameters
+    ----------
+    bending_moment : sympy.Expr
+        Bending moment expression.
+    x : sympy.Symbol
+        Independent variable.
+
+    Returns
+    -------
+    shear_force : sympy.Expr
+        Shear force expression.
+    """
     shear_force = -sympy.diff(bending_moment, x)
 
     return shear_force
